9a415b5392ac5cca8a46955233276259bdcf9795
shawnm4 <shawnm18057310@gmail.com>
2024-03-21 10:32:33 -0400

    styled table

--- src/Screens/DataTabel.tsx
+++ src/Screens/DataTabel.tsx
@@ -9,35 +9,90 @@ interface DataType {
   marketcap: number;
 }
 
-const columns: TableProps<DataType>["columns"] = [
-  {
-    title: "Rank",
-    dataIndex: "market_cap_rank",
-    key: "name",
-  },
-  {
-    title: "Name",
-    dataIndex: "name",
-    key: "name",
-  },
-  {
-    title: "Price",
-    dataIndex: "current_price",
-    key: "price",
-  },
-  {
-    title: "24 Hour Change",
-    key: "pricechange",
-    dataIndex: "price_change_24h",
-  },
-  {
-    title: "Market Cap",
-    key: "action",
-    dataIndex: "market_cap",
-  },
-];
+export default function DataTabel({ data, setActiveCoinForDashboard }: any) {
+  const columns: TableProps<DataType>["columns"] = [
+    {
+      title: "Rank",
+      key: "rank",
+      render: (record) => {
+        return (
+          <div
+            className=" text-xl"
+            onClick={() => setActiveCoinForDashboard(record)}
+          >
+            #{record.market_cap_rank}
+          </div>
+        );
+      },
+    },
+    {
+      title: "Name",
+      key: "name",
+      render: (record) => {
+        return (
+          <div
+            className="text-xl"
+            onClick={() => setActiveCoinForDashboard(record)}
+            style={{ whiteSpace: "nowrap" }}
+          >
+            <div className="flex w-2/12 gap-3">
+              <img src={`${record.image}`} />
+              <div className="self-center">{record.name}</div>
+            </div>
+          </div>
+        );
+      },
+    },
+    {
+      title: "Price",
+      key: "price",
+      render: (record) => {
+        return (
+          <div
+            className="text-xl"
+            onClick={() => setActiveCoinForDashboard(record)}
+          >
+            ${record.current_price}
+          </div>
+        );
+      },
+    },
+    {
+      title: "24 Hour Change",
+      key: "pricechange",
+      render: (record) => {
+        return (
+          <div
+            className="text-xl"
+            onClick={() => setActiveCoinForDashboard(record)}
+          >
+            {record.price_change_24h}
+          </div>
+        );
+      },
+    },
+    {
+      title: "Market Cap",
+      key: "action",
+      render: (record) => {
+        return (
+          <div
+            className="text-xl"
+            onClick={() => setActiveCoinForDashboard(record)}
+          >
+            ${record.market_cap}.
+          </div>
+        );
+      },
+    },
+  ];
 
-export default function DataTabel({ data }: { data: DataType[] }) {
-  console.log(data);
-  return <Table columns={columns} dataSource={data} pagination={false} />;
+  return (
+    <Table
+      scroll={{ y: 500 }}
+      columns={columns}
+      dataSource={data}
+      pagination={false}
+    />
+  );
 }
--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -3,9 +3,15 @@ import { DownOutlined, FilterFilled } from "@ant-design/icons";
 import type { MenuProps } from "antd";
 import DataTabel from "./DataTabel";
 import useGetCoinData from "../Hooks/useGetCoinData";
+import { useState } from "react";
 
 export default function Home() {
+  const [activeCoin, setActiveCoin] = useState(true);
   const { data: coinData } = useGetCoinData();
+
+  function setActiveCoinForDashboard(coinObj: any) {
+    setActiveCoin(coinObj);
+  }
 
   return (
     <>
@@ -16,27 +22,35 @@ export default function Home() {
         </h1>
       </header>
       <Divider />
-      <section className="flex justify-center">
-        <div className="w-5/6 rounded-md ">
-          <div className=" text-2xl ">
-            {" "}
-            <Dropdown menu={{ items }}>
-              <a onClick={(e) => e.preventDefault()}>
-                <Space>
-                  <div style={{ color: "#495057" }} className="">
-                    <FilterFilled />
-                  </div>
-                  Sort By
-                  <DownOutlined />
-                </Space>
-              </a>
-            </Dropdown>
+      {activeCoin ? (
+        <section className="flex justify-center mb-20">
+          <div className="w-5/6 rounded-md ">
+            <div className=" text-2xl ">
+              <Dropdown menu={{ items }}>
+                <a onClick={(e) => e.preventDefault()}>
+                  <Space>
+                    <div style={{ color: "#495057" }} className="">
+                      <FilterFilled />
+                    </div>
+                    Sort By
+                    <DownOutlined />
+                  </Space>
+                </a>
+              </Dropdown>
+            </div>
+            <div className=" cursor-pointer rounded-md shadow-xl mt-6 bg-gray-700">
+              <DataTabel
+                setActiveCoinForDashboard={setActiveCoinForDashboard}
+                data={coinData?.data}
+              />
+            </div>
           </div>
-          <div className=" rounded-md shadow-xl mt-6 bg-gray-700">
-            <DataTabel data={coinData?.data} />
-          </div>
-        </div>
-      </section>
+        </section>
+      ) : (
+        <section className="flex justify-center">
+          <div className="w-11/12 bg-slate-600"></div>
+        </section>
+      )}
     </>
   );
 }
