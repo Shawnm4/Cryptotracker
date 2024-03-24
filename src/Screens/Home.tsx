10ed3179599c99ae4e7f00209dd38272dad4995a
shawnm4 <shawnm18057310@gmail.com>
2024-03-24 14:12:07 -0400

    finished mobile home screen

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -4,6 +4,7 @@ import useGetCoinData from "../Hooks/useGetAllCoinData";
 import { useState } from "react";
 import CoinDashboard from "./CoinDashboard";
 import { EColors } from "../Enums/EColors";
+import { RiseOutlined, FallOutlined } from "@ant-design/icons";
 
 export default function Home() {
   interface CoinObject {
@@ -22,43 +23,138 @@ export default function Home() {
 
   return (
     <>
-      <header
-        style={{
-          backgroundColor: EColors.SECONDARYBACKGROUND,
-          borderColor: EColors.BORDERGRAY,
-        }}
-        className="flex justify-center  items-center font-medium border-b  "
-      >
-        <h1
-          style={{ color: "white", fontFamily: "Inter", fontWeight: 200 }}
-          className="text-3xl text-center"
+      <div className="hidden  2xl:inline-block">
+        <header
+          style={{
+            backgroundColor: EColors.SECONDARYBACKGROUND,
+            borderColor: EColors.BORDERGRAY,
+          }}
+          className="flex justify-center  items-center font-medium border-b  "
         >
-          <div>
-            Explore the Top 50 Cryptocurrencies Rankings & Real-Time Market Data
-          </div>
-        </h1>
-      </header>
+          <h1
+            style={{ color: "white", fontFamily: "Inter", fontWeight: 200 }}
+            className="text-3xl text-center"
+          >
+            <div>
+              Explore the Top 50 Cryptocurrencies Rankings & Real-Time Market
+              Data
+            </div>
+          </h1>
+        </header>
 
-      {!activeCoin ? (
-        <section
-          style={{ backgroundColor: "black" }}
-          className="flex justify-center h-screen"
+        {!activeCoin ? (
+          <section
+            style={{ backgroundColor: "black" }}
+            className="flex justify-center h-screen"
+          >
+            <div className="w-5/6 rounded-md ">
+              <div className=" cursor-pointer rounded-md shadow-xl mt-10 bg-red-700">
+                <DataTabel
+                  setActiveCoinForDashboard={setActiveCoinForDashboard}
+                  data={coinData?.data}
+                />
+              </div>
+            </div>
+          </section>
+        ) : (
+          <CoinDashboard
+            activeCoin={activeCoin}
+            clearActiveCoin={clearActiveCoin}
+          />
+        )}
+      </div>
+      {/* Tablet */}
+      <div className=" hidden md:flex 2xl:inline-block">
+        <header
+          style={{
+            backgroundColor: EColors.SECONDARYBACKGROUND,
+            borderColor: EColors.BORDERGRAY,
+          }}
+          className="flex w-full justify-center  items-center font-medium border-b  "
         >
-          <div className="w-5/6 rounded-md ">
-            <div className=" cursor-pointer rounded-md shadow-xl mt-10 bg-red-700">
-              <DataTabel
-                setActiveCoinForDashboard={setActiveCoinForDashboard}
-                data={coinData?.data}
-              />
+          <h1
+            style={{ color: "white", fontFamily: "Inter", fontWeight: 200 }}
+            className="text-3xl text-center"
+          >
+            <div>
+              Explore the Top 50 Cryptocurrencies Rankings & Real-Time Market
+              Data
+            </div>
+          </h1>
+        </header>
+      </div>
+
+      {/* CellPhone */}
+      <div className="md:hidden">
+        <header
+          style={{
+            backgroundColor: EColors.SECONDARYBACKGROUND,
+            borderColor: EColors.BORDERGRAY,
+          }}
+          className="flex justify-center  items-center font-medium border-b  "
+        >
+          <h1
+            style={{ color: "white", fontFamily: "Inter", fontWeight: 200 }}
+            className="text-3xl text-center"
+          >
+            <div>
+              Explore the Top 50 Cryptocurrencies Rankings & Real-Time Market
+              Data
+            </div>
+          </h1>
+        </header>
+        <div style={{ backgroundColor: EColors.PRIMARYBACKGROUND }}>
+          {coinData?.data?.map((coin: any) => (
+            <CoinListItem coinData={coin} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+function CoinListItem({ coinData }: any) {
+  return (
+    <>
+      <div className="border-b border-white " style={{ color: "white" }}>
+        <div className="flex mx-4 gap-6 p-3">
+          <div className="font-bold text-lg">#{coinData.market_cap_rank}</div>
+          <div className="flex gap-2 ">
+            <div className="w-2/12">
+              <img src={coinData.image} />
+            </div>
+            <div>
+              <div className=" font-bold text-lg">
+                {coinData.name.toUpperCase()}
+              </div>
+              <div className="">{coinData.symbol.toUpperCase()}</div>
             </div>
           </div>
-        </section>
-      ) : (
-        <CoinDashboard
-          activeCoin={activeCoin}
-          clearActiveCoin={clearActiveCoin}
-        />
-      )}
+          <div>
+            <div className="text-lg">${coinData.current_price.toFixed(2)}</div>
+            <div className="text-lg">
+              {coinData.price_change_percentage_24h > 0 ? (
+                <div
+                  style={{ color: EColors.PRIMARY }}
+                  className="flex gap-1 text-xl"
+                >
+                  <div>{coinData.price_change_percentage_24h.toFixed(2)}%</div>
+                  <div>
+                    <RiseOutlined />
+                  </div>
+                </div>
+              ) : (
+                <div style={{ color: "red" }} className="flex gap-1 text-xl">
+                  <div>{coinData.price_change_percentage_24h.toFixed(2)}%</div>
+                  <div style={{ color: "red" }}>
+                    <FallOutlined />
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/Screens/TopNavBar.tsx
+++ src/Screens/TopNavBar.tsx
@@ -7,8 +7,9 @@ export default function TopNavBar() {
         style={{
           backgroundColor: EColors.PRIMARYBACKGROUND,
           borderColor: EColors.BORDERGRAY,
+          filter: "saturate(-30%)",
         }}
-        className="h-32 flex items-center justify-center border-b border-white "
+        className="hidden  2xl:flex h-32 items-center justify-center border-b border-white "
       >
         <div
           style={{ fontFamily: "Orbitron", color: EColors.PRIMARY }}
@@ -17,6 +18,37 @@ export default function TopNavBar() {
           <div className="flex">
             <div>Crypto</div>
             <div className="text-white">Tracker</div>
+          </div>
+        </div>
+      </nav>
+      {/* Tablet */}
+      <nav className=" hidden md:flex 2xl:hidden">
+        <div
+          style={{
+            backgroundColor: EColors.PRIMARYBACKGROUND,
+            borderColor: EColors.BORDERGRAY,
+          }}
+          className="flex w-full justify-center items-center border-b border-white  h-10"
+        >
+          <div className=" flex text-2xl" style={{ fontFamily: "Orbitron" }}>
+            <div style={{ color: EColors.PRIMARY }}>Crypto</div>
+            <div style={{ color: "white" }}>Tracker</div>
+          </div>
+        </div>
+      </nav>
+
+      {/* CellPhone */}
+      <nav className="md:hidden">
+        <div
+          style={{
+            backgroundColor: EColors.PRIMARYBACKGROUND,
+            borderColor: EColors.BORDERGRAY,
+          }}
+          className="flex justify-center items-center border-b border-white  h-10"
+        >
+          <div className="flex text-2xl" style={{ fontFamily: "Orbitron" }}>
+            <div style={{ color: EColors.PRIMARY }}>Crypto</div>
+            <div style={{ color: "white" }}>Tracker</div>
           </div>
         </div>
       </nav>
